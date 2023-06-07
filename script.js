5321bcd1329b6716acef0c65f4d0be7e032ab6b8
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-06-07 14:09:36 +0300

    Add password matching validation

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// 
+let pw = document.querySelector("#password");
+let confirm = document.querySelector("#confirm");
+let message = document.querySelector(".message");
+
+confirm.addEventListener('keyup', (e) => {
+    if (pw.value == confirm.value) 
+    {
+        confirm.setCustomValidity("");
+        message.textContent = "";
+        console.log(confirm.validity);
+    }
+    else {
+        confirm.setCustomValidity("Passwords don't match.");
+        message.textContent = "Passwords don't match";
+        console.log(confirm.validity);
+    }
+
+});
